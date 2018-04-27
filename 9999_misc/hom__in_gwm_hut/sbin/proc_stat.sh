31d0222ca512fc2adb0fbf982bbf27ff0980bf7b
shaobo luo <shaobo.luo@continental-corporation.com>
2018-04-27 16:49:29 +0800

    update

--- 9999_misc/hom__in_gwm_hut/sbin/proc_stat.sh
+++ 9999_misc/hom__in_gwm_hut/sbin/proc_stat.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+#echo "output line info: cmd, pid, ppid, pgid, sid, tty_dev, tty_pgid"
+
 PROCS=`ls -d /proc/*/`
 
 for P in $PROCS; do
     if [ ! -f $P/stat ]; then
         continue
     fi
-    cat $P/stat | awk '{ print $2,$1,$4,$5,$6 }'
+    cat $P/stat | awk '{ print $2,$1,$4,$5,$6,$7,$8 }'
     
     TASTKS=`ls -d $P/task/*/`
     for T in $TASTKS; do
         if [ ! -f $T/stat ]; then
             continue
         fi
-        cat $T/stat | awk '{ print "  ", $2,$1,$4,$5,$6 }'
+        cat $T/stat | awk '{ print "  ", $2,$1,$4,$5,$6,$7,$8 }'
     done
 done
